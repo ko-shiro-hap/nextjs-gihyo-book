6e3ed667de413379d3990f8b74ab36d74a412879
ko-shiro-hap <koushirou.iino@icloud.com>
2022-12-25 13:13:34 +0900

    themeを用意

---
+++ src/themes/space.ts
@@ -0,0 +1,10 @@
+import { Space_Grotesk } from "@next/font/google"
+
+const space: any = ['0px', '8px', '16px', '32px', '64px']
+
+//alias
+space.small = space[1]
+space.small = space[2]
+space.small = space[3]
+
+export default space
